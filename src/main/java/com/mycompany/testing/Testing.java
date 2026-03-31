3c0038a23e2cdce3e649bfd05cff1b558bbdd19e
ST10514949-Wandile-Sandlane <ST10514949@myemeris.edu.za>
2026-03-31 10:48:35 +0200

    first push

---
+++ src/main/java/com/mycompany/testing/Testing.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.testing;
+
+/**
+ *
+ * @author lab_services_student
+ */
+public class Testing {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        // testing 
+    }
+}
